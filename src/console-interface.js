fd8856a0918182ef348c0772af72c66bb65ead18
khayri <khayriramzy@gmail.com>
2026-08-22 02:54:22 -0400

    Create pseudocode for project and console interface

---
+++ src/console-interface.js
@@ -0,0 +1,7 @@
+// print out all projects available, along with its todo items (title & dueDate)
+// there is one project by default
+
+// menu should have:
+// 1. show projects (print projects out)
+// 2. expand project (input which project to see/edit its details)
+// 3. delete todo from project
---
+++ src/project.js
@@ -0,0 +1,7 @@
+// projects are where todo items are contained
+// projects can have multiple todo items
+// ex: college project can have "finish calculus homework" item, "study for programming exam" item, etc
+// has priority property
+// has an array to contain todo items
+// show todo items on the surface (title, duedate, priority. color of project changes on priority)
+// show all todo items details (title, duedate, etc)
