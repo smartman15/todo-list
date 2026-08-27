c6ded4a8c6d9a88719cb4a429651578e0889e670
khayri <khayriramzy@gmail.com>
2026-08-27 07:50:21 -0400

    Create class projectList pseudocode to store projects

---
+++ src/project-list.js
@@ -0,0 +1,29 @@
+// project list is where all projects are stored
+    // array to store projects
+
+    // function to show all projects and brief details of todo items
+        // print index
+        // print project name and priority
+        // print basic details of todo items (title, duedate, priority)
+        
+    // function to remove project
+    // function takes in index as parameter
+        // if index is valid
+            // remove project at index i in array of project list
+            // print success
+        
+        // else 
+            // print invalid index
+
+        
+    // function to edit project name and priority
+    // function takes in index as parameter
+        // if index is valid
+            // ask input for name
+            // ask input for priority
+
+            // set project name to input using project function
+            // set project priority to input using project function
+
+        // if index is invalid
+            // print index invalid
