09c811cd193459b333fb28d444e82d3e5f49b7aa
khayri <khayriramzy@gmail.com>
2026-08-29 05:51:28 -0400

    Create function to add project to project list

--- src/project-list.js
+++ src/project-list.js
@@ -1,19 +1,27 @@
 // project list is where all projects are stored
     // array to store projects
 
+
+    // function to create project
+    // takes in name, priority as parameters
+        // using project constructor, create project
+        // store project in projects array
+
+
+    // function to show all project names
+        // loop through projects array
+            // print project name
+
+
     // function to show all projects and brief details of todo items
         // print index
         // print project name and priority
         // print basic details of todo items (title, duedate, priority)
-        
-    // function to remove project
-    // function takes in index as parameter
-        // if index is valid
-            // remove project at index i in array of project list
-            // print success
-        
-        // else 
-            // print invalid index
+    
+
+    // function to show details of project at specific index
+    // takes in index i as parameter
+        // print details of project at array[i] using project function
 
         
     // function to edit project name and priority
@@ -26,4 +34,14 @@
             // set project priority to input using project function
 
         // if index is invalid
-            // print index invalid
+            // log index invalid
+
+
+    // function to remove project
+    // function takes in index as parameter
+        // if index is valid
+            // remove project at index i in array of project list
+            // log success  
+        
+        // else 
+            // log invalid index
