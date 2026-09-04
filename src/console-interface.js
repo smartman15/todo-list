354ba0f99d7bdedc6838f8aa835c16be40fb5446
khayri <khayriramzy@gmail.com>
2026-09-04 06:33:18 -0400

    Create pseudocode for console interface

--- src/console-interface.js
+++ src/console-interface.js
@@ -2,6 +2,38 @@
 // there is one project by default
 
 // menu should have:
-// 1. show projects (print projects out)
-// 2. expand project (input which project to see/edit its details)
+// 1. show projects (print projects out using projectList function)
+// 2. expand project to see/edit todos
+
+
 // 3. delete todo from project
+
+
+
+
+// 2. expand project to see/edit todos
+    // print all todos using project function
+
+    // option:
+    // 1. edit todo
+        // input index of todo
+        // verify if index is valid using index-validator
+            // input title, description, dueDate, priority
+            // at items[i] of project,
+            // use setters to set
+            // title, description, dueDate, priority
+
+        // else
+            // print index is invalid
+
+    // 2. exit
+
+
+// 3. delete todo from project
+    // ask index input from user
+    // use index as parameter value to remove todo item function from project.js
+    // if index is valid
+        // use remove project function from project-list
+
+    // else 
+        // print index is invalid
