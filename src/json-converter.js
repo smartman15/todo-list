72afda441d948a9aef7213012490ecc7ccc52bfa
khayri <khayriramzy@gmail.com>
2026-09-13 00:59:00 -0300

    Create convertoToProject, convertToTodo, and a part of storeLocalStorage and loadLocalStorage functions

--- src/json-converter.js
+++ src/json-converter.js
@@ -1,41 +1,78 @@
 // this module converts values to json and json back to their original values
 // the purpose of this is to be able to use localStorage API
 
-// STORE IN LOCALSTORAGE
-    // jsonConverter takes in projectList array
-    // converts to json
-    // store into localStorage
+import { Project } from "./project";
+import { projectList } from "./project-list"
+import { TodoItem } from "./todo-item";
+
+const jsonConverter = (() => {
+    // STORE IN LOCALSTORAGE
+    const storeLocalStorage = () => {
+        // take in projectList array
+        // converts to json
+        // store into localStorage
+        localStorage.setItem("array", JSON.stringify(projectList.getProjects()));
+    }
+        
 
 
-// LOAD FROM LOCALSTORAGE
-    // take projectList array from localStorage
-    // convert projectList array back to array of objects (use json parse())
-    // store into projectList array in code
-    // execute convertToProject() function
-    // loop through projectList array
-        // execute convertToTodo() and pass projectList array[i] as parameter value
+    // LOAD FROM LOCALSTORAGE
+    const loadLocalStorage = () => {
+        // take projectList array from localStorage
+        // convert projectList array back to array of objects (use json parse())
+        // store into projectList array in code
+        projectList.getProjects().push(JSON.parse(localStorage.getItem("array")));
+        // execute convertToProject() function
+        // loop through projectList array
+            // execute convertToTodo() and pass projectList array[i] as parameter value
+    }
+        
 
 
-// function to stringify projectList array and store into localStorage
-    // json stringify projectList array
-    // store into localStorage by using localStorage setItem() function
+    // function to stringify projectList array and store into localStorage
+        // json stringify projectList array
+        // store into localStorage by using localStorage setItem() function
 
 
-// function convertToProject() to turn parsed json into Project Object
-    // loop through projectList array
-    // for every project element in projectList array
-        // create jsonProject variable
-        // json.parse() the project element and assign it to jsonProject variable
-        // create new Project object and assign it to project variable (leave parameters empty)
-        // set project.data to the parsed json
-        // replace element at current index with project variable
+    // function convertToProject() to turn parsed json into Project Object
+    const convertToProject = () => {
+        // loop through projectList array
+        // for every project element in projectList array
+        let array = projectList.getProjects();
+        for(let i = 0; i < array.length; i++){
+            // create jsonProject variable
+            // json.parse() the project element and assign it to jsonProject variable
+            let jsonProject = JSON.parse(array[i]);
+            // create new Project object and assign it to project variable (leave parameters empty)
+            let project = new Project();
+            // set project.data to the parsed json project
+            project.data = jsonProject._data;
+            // replace element at current index with project variable
+            array[i] = project;
+        }
+            
+    }
+        
 
 
-// function convertToTodo() to turn parsed json into TodoItem Object
-// parameter takes in Project object
-    // for every todo element in Project item array
-        // create jsonTodo variable
-        // json.parse() the todo element and store it in jsonTodo variable
-        // create new TodoItem object and assign it to todo variable (leave parameters empty)
-        // set todo.data to the parsed json
-        // replace todo element at current index with todo variable
+    // function convertToTodo() to turn parsed json into TodoItem Object
+    // parameter takes in Project object
+    const convertToTodo = (project) => {
+        // for every todo element in Project items array
+        let projectItems = project.items;
+        for(let i = 0; i < projectItems.length; i++){
+            // create jsonTodo variable
+            // json.parse() the todo element and store it in jsonTodo variable
+            let jsonTodo = JSON.parse(projectItems[i]);
+            // create new TodoItem object and assign it to todo variable (leave parameters empty)
+            let todoItem = new TodoItem();
+            // set todo.data to the parsed json
+            todoItem.data = jsonTodo._data;
+            // replace todo element at current index with todo variable
+            projectItems[i] = todoItem;
+        } 
+            
+    }
+        
+})();
+
