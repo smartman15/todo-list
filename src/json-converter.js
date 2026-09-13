e1b0e54a263785767d49abbdee00f5cc746bd18f
khayri <khayriramzy@gmail.com>
2026-09-13 00:37:47 -0300

    Create pseudocode for converting parsed json to TodoItem objects, and create more steps to the load from localstorage function

--- src/json-converter.js
+++ src/json-converter.js
@@ -2,15 +2,18 @@
 // the purpose of this is to be able to use localStorage API
 
 // STORE IN LOCALSTORAGE
-// jsonConverter takes in projectList array
-// converts to json
-// store into localStorage
+    // jsonConverter takes in projectList array
+    // converts to json
+    // store into localStorage
 
 
 // LOAD FROM LOCALSTORAGE
-// take projectList array from localStorage
-// convert projectList array back to array of objects (use json parse())
-// store into projectList array in code
+    // take projectList array from localStorage
+    // convert projectList array back to array of objects (use json parse())
+    // store into projectList array in code
+    // execute convertToProject() function
+    // loop through projectList array
+        // execute convertToTodo() and pass projectList array[i] as parameter value
 
 
 // function to stringify projectList array and store into localStorage
@@ -18,11 +21,21 @@
     // store into localStorage by using localStorage setItem() function
 
 
-// function to turn parsed json into project Object
+// function convertToProject() to turn parsed json into Project Object
     // loop through projectList array
     // for every project element in projectList array
         // create jsonProject variable
-        // json.parse() the project and assign it to jsonProject variable
+        // json.parse() the project element and assign it to jsonProject variable
         // create new Project object and assign it to project variable (leave parameters empty)
         // set project.data to the parsed json
         // replace element at current index with project variable
+
+
+// function convertToTodo() to turn parsed json into TodoItem Object
+// parameter takes in Project object
+    // for every todo element in Project item array
+        // create jsonTodo variable
+        // json.parse() the todo element and store it in jsonTodo variable
+        // create new TodoItem object and assign it to todo variable (leave parameters empty)
+        // set todo.data to the parsed json
+        // replace todo element at current index with todo variable
