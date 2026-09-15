2fb501b699043e98ed3d5c8e51d9a757692255ec
khayri <khayriramzy@gmail.com>
2026-09-15 07:27:09 -0300

    Create pseudocode for loading localStorage in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { consoleInterface } from "./console-interface.js";
 import { jsonConverter } from "./json-converter.js";
 
+// load localStorage
+// if localStorage of projectList exists
+    // load localStorage
+    // execute consoleInterface()
+    
+// else 
+    // execute consoleInterface()
+
 consoleInterface();
