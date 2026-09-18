4bc879666ecbd4f79fc342ab53474c3d66531a43
khayri <khayriramzy@gmail.com>
2026-09-18 01:09:24 -0300

    Create if statement in case localStorage doesnt exist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import { consoleInterface } from "./console-interface.js";
 import { jsonConverter } from "./json-converter.js";
 
+// localStorage.setItem("test", JSON.stringify(["what"]));
+
 // load localStorage
 // if localStorage of projectList exists
-    // load localStorage
+if(localStorage.getItem("array")){
+    // load localStorag
+    jsonConverter.loadLocalStorage();
     // execute consoleInterface()
+    consoleInterface.startInterface();
+}
+else{
+    // execute consoleInterface()
+    consoleInterface.startInterface();
+}
     
-// else 
-    // execute consoleInterface()
 
-jsonConverter.loadLocalStorage();
-consoleInterface();
+    
+
