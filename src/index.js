8ea2d5cad683abe6fd713d9a0b22a580fca99ffe
khayri <khayriramzy@gmail.com>
2026-09-18 04:23:24 -0300

    Test out html by loading projectContainer()

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import { consoleInterface } from "./console-interface.js";
 import { jsonConverter } from "./json-converter.js";
+import { projectContainer } from "./dom/project-container.js";
 
 // localStorage.setItem("test", JSON.stringify(["what"]));
 
 // load localStorage
 // if localStorage of projectList exists
-if(localStorage.getItem("array")){
-    // load localStorag
-    jsonConverter.loadLocalStorage();
-    // execute consoleInterface()
-    consoleInterface.startInterface();
-}
-else{
-    // execute consoleInterface()
-    consoleInterface.startInterface();
-}
+// if(localStorage.getItem("array")){
+//     // load localStorag
+//     jsonConverter.loadLocalStorage();
+//     // execute consoleInterface()
+//     consoleInterface.startInterface();
+// }
+// else{
+//     // execute consoleInterface()
+//     consoleInterface.startInterface();
+// }
     
-
+jsonConverter.loadLocalStorage();
+projectContainer();
     
 
