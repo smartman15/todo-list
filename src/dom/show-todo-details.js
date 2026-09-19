01ef995a5eee8dc6f3c9c5809198f7baeae44afe
khayri <khayriramzy@gmail.com>
2026-09-19 07:06:33 -0300

    Create function that hides/shows todo details (which for some reason isnt working yet)

--- src/dom/show-todo-details.js
+++ src/dom/show-todo-details.js
@@ -1,7 +1,17 @@
-export function showTodoDetails(){
-    // get button by its class show-details
-    // if todo details are visible
-        // hide todo details
-    // else if todo details are hidden
-        // set todo details to visible
+export function showTodoDetails(todo){
+    // let todo = document.querySelector('.todo-details');
+
+    // if todo details are hidden
+    if(todo.style.visibility == 'hidden'){
+        // show todo details
+        todo.style.visibility = 'visible';
+    }
+        
+    // else if todo details are visible
+    else{
+    // set todo details to hidden
+        todo.style.visibility = 'hidden'
+    }
 }
+    
+        
