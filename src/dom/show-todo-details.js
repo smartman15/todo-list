628f1fadc09a3ad6a8252677fe5023920475dd4c
khayri <khayriramzy@gmail.com>
2026-09-19 06:39:11 -0300

    Create function for buttonto show and hide todo details

---
+++ src/dom/show-todo-details.js
@@ -0,0 +1,7 @@
+export function showTodoDetails(){
+    // get button by its class show-details
+    // if todo details are visible
+        // hide todo details
+    // else if todo details are hidden
+        // set todo details to visible
+}
