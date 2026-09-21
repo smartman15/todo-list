c24bbe9ae6ee59060e864d0313a7eba7ee35c002
khayri <khayriramzy@gmail.com>
2026-09-20 21:20:09 -0300

    Refactor console options into functions

--- src/console-interface.js
+++ src/console-interface.js
@@ -12,6 +12,153 @@ import { jsonConverter } from "./json-converter.js";
 // 2. expand project to see/edit todos
 // 3. delete todo from project
 export const consoleInterface = (() => {
+    const exitInterface = () => {
+        logMessage('goodbye');
+        return;
+    };
+
+    const showProjects = () => {
+        projectList.showProjectNames();
+    }
+
+    const showTodos = () => {
+        // ask index of which project the user would like to see the todos of
+        let index = prompt("Insert index of project");
+
+        // if index is valid
+        if(isValidIndex(projectList.getProjects(), index)){
+            let project = projectList.getProjects()[index];
+             // show todos of project
+             project.ShowAllTodoDetail();
+             
+        }
+           
+        
+        // else
+        else logMessage("Invalid index");
+            // print invalid index
+    }
+
+
+    const editTodo = () => {
+        // input index of project
+        let index = prompt("Insert index of project");
+        // verify if index is valid
+        if(isValidIndex(projectList.getProjects(), index)){
+            // create variable to store project at index
+            let project = projectList.getProjects()[index];
+
+            // print todos of selected project
+            project.showBasicTodoDetail();
+
+            // input index of todo
+            let todoIdx = prompt("Insert index of todo to edit");
+            // verify if index is valid
+            if(isValidIndex(project.items, todoIdx)){
+                let todo = project.items[todoIdx];
+                // input title, description, dueDate, priority, complete (set default input as current property values)
+                let title = prompt("Insert new title", todo.title);
+                let desc = prompt("Insert new description", todo.description);
+                let dueDate = prompt("Insert new due date", todo.dueDate);
+                let priority = prompt("Insert new priority", todo.priority);
+                let complete = prompt("Insert new completion status", todo.complete);
+                // at items[i] of project,
+                // use setters to set
+                // title, description, dueDate, priority, complete
+                todo.title = title;
+                todo.description = desc;
+                todo.dueDate = dueDate;
+                todo.priority = priority;
+                todo.complete = complete;
+                logMessage("Todo successfully updated");
+                jsonConverter.storeLocalStorage();
+            }
+                
+
+            // else
+            else logMessage("Invalid index");
+                // print index is invalid
+        }
+    }
+
+    const createTodo = () => {
+        // ask user to input project index to choose which project to add todo
+        let projectIdx = prompt("Insert index of project to add todo to");
+        // verify if index is valid
+        if(isValidIndex(projectList.getProjects(), projectIdx)){
+            // create variable to store project at index
+            let project = projectList.getProjects()[projectIdx];
+            // ask user to input todo title, description, dueDate, priority
+            let title = prompt("Insert title of todo", "water the plants");
+            let description = prompt("Insert description of todo", "water all plants that are in the garden");
+            let dueDate = prompt("Insert due date of todo");
+            let priority = prompt("Insert urgency of todo", "not urgent");
+            // create todo item with input values
+            // let todo = new TodoItem(title, description, dueDate, priority);
+            // insert todo item into project variable
+            project.addTodo(title, description, dueDate, priority);
+            jsonConverter.storeLocalStorage();
+        }
+
+        else logMessage("Invalid index");
+    }
+
+    
+    const createProject = () => {
+        // ask user to input project name and priority
+        let projectName = prompt("Enter project name", "College");
+        let projectPriority = prompt("Enter project priority", "not urgent");
+        // create Project with input values
+        let project = new Project(projectName, projectPriority);
+        // insert project into project list
+        projectList.getProjects().push(project);
+        jsonConverter.storeLocalStorage();
+    }
+
+
+    const deleteTodo = () => {
+        // print all projects
+        projectList.showProjectNames();
+        // ask user to input project index
+        let projectIdx = prompt("Insert project index to delete todo from");
+        // if index is valid
+        if(isValidIndex(projectList.getProjects(), projectIdx)){
+            // create variable to store project at index
+            let project = projectList.getProjects()[projectIdx];
+            // print todo items of project variable
+            project.showBasicTodoDetail();
+            // ask user to input todo index to delete
+            let todoIdx = prompt("Insert index of todo to delete");
+            // if index is valid
+            if(isValidIndex(project.items, todoIdx)){
+                // use index as parameter value to remove todo item from project
+                project.removeTodo(todoIdx);
+                jsonConverter.storeLocalStorage();
+            }
+                
+            
+            // else
+            else logMessage("Invalid index");
+                // print index is invalid
+        }
+            
+            
+
+        // else 
+        else logMessage("Invalid index");
+            // print index is invalid
+    }
+
+
+    const deleteProject = () => {
+        // ask user to input index of project to delete
+        let index = prompt("Insert index of project to delete");
+        // execute removeProject() of projectList
+        projectList.removeProject(index);
+        jsonConverter.storeLocalStorage();
+    }
+
+
     const startInterface = () => {
         while (true){
             logMessage(" ");
@@ -28,156 +175,45 @@ export const consoleInterface = (() => {
     
             // 0. exit
             if(option == 0){
-                logMessage('goodbye');
-                break;
+                exitInterface();
             }
     
             // 1. show projects (print projects out using projectList function)
             else if(option == 1){
-                projectList.showProjectNames();
+                showProjects();
             }
 
             // 2. show todos )
             else if(option == 2){
-                // ask index of which project the user would like to see the todos of
-                let index = prompt("Insert index of project");
-
-                // if index is valid
-                if(isValidIndex(projectList.getProjects(), index)){
-                    let project = projectList.getProjects()[index];
-                     // show todos of project
-                     project.ShowAllTodoDetail();
-                     
-                }
-                   
-                
-                // else
-                else logMessage("Invalid index");
-                    // print invalid index
+                showTodos();
             }
     
             // 2. expand project to see/edit todos
             else if(option == 3){
-                // input index of project
-                let index = prompt("Insert index of project");
-                // verify if index is valid
-                if(isValidIndex(projectList.getProjects(), index)){
-                    // create variable to store project at index
-                    let project = projectList.getProjects()[index];
-    
-                    // print todos of selected project
-                    project.showBasicTodoDetail();
-    
-                    // input index of todo
-                    let todoIdx = prompt("Insert index of todo to edit");
-                    // verify if index is valid
-                    if(isValidIndex(project.items, todoIdx)){
-                        let todo = project.items[todoIdx];
-                        // input title, description, dueDate, priority, complete (set default input as current property values)
-                        let title = prompt("Insert new title", todo.title);
-                        let desc = prompt("Insert new description", todo.description);
-                        let dueDate = prompt("Insert new due date", todo.dueDate);
-                        let priority = prompt("Insert new priority", todo.priority);
-                        let complete = prompt("Insert new completion status", todo.complete);
-                        // at items[i] of project,
-                        // use setters to set
-                        // title, description, dueDate, priority, complete
-                        todo.title = title;
-                        todo.description = desc;
-                        todo.dueDate = dueDate;
-                        todo.priority = priority;
-                        todo.complete = complete;
-                        logMessage("Todo successfully updated");
-                        jsonConverter.storeLocalStorage();
-                    }
-                        
-    
-                    // else
-                    else logMessage("Invalid index");
-                        // print index is invalid
-                }
+                editTodo();
             }
     
             // 3. create new project
             else if(option == 4){
-                // ask user to input project name and priority
-                let projectName = prompt("Enter project name", "College");
-                let projectPriority = prompt("Enter project priority", "not urgent");
-                // create Project with input values
-                let project = new Project(projectName, projectPriority);
-                // insert project into project list
-                projectList.getProjects().push(project);
-                jsonConverter.storeLocalStorage();
+                createProject();
             }
                 
     
             // 4. add todo to project
             else if(option == 5){
-                // ask user to input project index to choose which project to add todo
-                let projectIdx = prompt("Insert index of project to add todo to");
-                // verify if index is valid
-                if(isValidIndex(projectList.getProjects(), projectIdx)){
-                    // create variable to store project at index
-                    let project = projectList.getProjects()[projectIdx];
-                    // ask user to input todo title, description, dueDate, priority
-                    let title = prompt("Insert title of todo", "water the plants");
-                    let description = prompt("Insert description of todo", "water all plants that are in the garden");
-                    let dueDate = prompt("Insert due date of todo");
-                    let priority = prompt("Insert urgency of todo", "not urgent");
-                    // create todo item with input values
-                    // let todo = new TodoItem(title, description, dueDate, priority);
-                    // insert todo item into project variable
-                    project.addTodo(title, description, dueDate, priority);
-                    jsonConverter.storeLocalStorage();
-                }
-    
-                else logMessage("Invalid index");
-                    
+                createTodo();
             }
                 
     
     
             // 5. delete todo from project
             else if(option == 6){
-                // print all projects
-                projectList.showProjectNames();
-                // ask user to input project index
-                let projectIdx = prompt("Insert project index to delete todo from");
-                // if index is valid
-                if(isValidIndex(projectList.getProjects(), projectIdx)){
-                    // create variable to store project at index
-                    let project = projectList.getProjects()[projectIdx];
-                    // print todo items of project variable
-                    project.showBasicTodoDetail();
-                    // ask user to input todo index to delete
-                    let todoIdx = prompt("Insert index of todo to delete");
-                    // if index is valid
-                    if(isValidIndex(project.items, todoIdx)){
-                        // use index as parameter value to remove todo item from project
-                        project.removeTodo(todoIdx);
-                        jsonConverter.storeLocalStorage();
-                    }
-                        
-                    
-                    // else
-                    else logMessage("Invalid index");
-                        // print index is invalid
-                }
-                    
-                    
-    
-                // else 
-                else logMessage("Invalid index");
-                    // print index is invalid
+                deleteTodo();
             }
 
             // 7. delete project
             else if(option == 7){
-                // ask user to input index of project to delete
-                let index = prompt("Insert index of project to delete");
-                // execute removeProject() of projectList
-                projectList.removeProject(index);
-                jsonConverter.storeLocalStorage();
+                deleteProject();
             }
 
         }
