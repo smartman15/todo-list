e3043060ee17b897c3ed354f17b5aae522e2c65d
khayri <khayriramzy@gmail.com>
2026-09-20 22:09:50 -0300

    Create click button function for creating new project and loading it into dom

---
+++ src/dom/create-project-btn.js
@@ -0,0 +1,13 @@
+ import { consoleInterface } from "../console-interface.js";
+ import { projectContainer } from "./project-container.js";
+
+export function createProject(){
+    // get content div from template.html
+    const contentDiv = document.querySelector('#content');
+
+    const projectContainer = document.createElement('div');
+    const createBtn = document.getElementById('create-project');
+    createBtn.addEventListener("click", consoleInterface.createProject);
+    
+    projectContainer();
+}
