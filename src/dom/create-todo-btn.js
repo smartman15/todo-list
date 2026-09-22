dd5dfb480e37d00df0a50fd5cd0602943337ce49
khayri <khayriramzy@gmail.com>
2026-09-22 09:31:54 -0300

    Remove textContent because it's redundant

--- src/dom/create-todo-btn.js
+++ src/dom/create-todo-btn.js
@@ -6,7 +6,6 @@ export function createTodoBtn(){
 
     // create button
     const todoBtn = document.getElementById('create-todo');
-    todoBtn.textContent = "Create Todo";
     // add click event listener, createTodo() of console interface
     todoBtn.addEventListener('click', () =>{
         consoleInterface.createTodo();
--- src/dom/edit-todo-btn.js
+++ src/dom/edit-todo-btn.js
@@ -6,7 +6,6 @@ export function editTodo(){
 
     // create button
     const editBtn = document.getElementById('edit-todo');
-    editBtn.textContent = "Create Todo";
     // add click event listener, createTodo() of console interface
     editBtn.addEventListener('click', () =>{
         consoleInterface.editTodo();
