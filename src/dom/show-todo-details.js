53e4ace14c584e8952fd000c88c7d31f81a4bed4
khayri <khayriramzy@gmail.com>
2026-09-22 07:39:18 -0300

    Add another equal sign for visiblity check

--- src/dom/show-todo-details.js
+++ src/dom/show-todo-details.js
@@ -1,16 +1,16 @@
-export function showTodoDetails(todo){
+export function showTodoDetails(e){
     // let todo = document.querySelector('.todo-details');
 
     // if todo details are hidden
-    if(todo.style.visibility == 'hidden'){
+    if(todoDetails.style.visibility === 'hidden'){
         // show todo details
-        todo.style.visibility = 'visible';
+        todoDetails.style.visibility = 'visible';
     }
         
     // else if todo details are visible
     else{
     // set todo details to hidden
-        todo.style.visibility = 'hidden'
+        todoDetails.style.visibility = 'hidden'
     }
 }
     
